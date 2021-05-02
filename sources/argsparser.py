65515aeccf74698caa037a09eb13d3c025ebaa49
ElSicarius <amurat654@gmail.com>
2021-05-02 12:36:35 +0200

    adding streaming lang

--- sources/argsparser.py
+++ sources/argsparser.py
@@ -9,5 +9,6 @@ def get_arguments():
     """
     parser = argparse.ArgumentParser(description='', epilog="")
     parser.add_argument('title', help='Movie title', default=None)
+    parser.add_argument('-l',"--lang", help='language of the streaming', default="fr")
     args = parser.parse_args()
     return args
