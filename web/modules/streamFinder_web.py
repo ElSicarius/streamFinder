9849018d77ea1945cf748b709b71e43f3dbeb6a3
ElSicarius <amurat654@gmail.com>
2021-05-12 12:32:39 +0200

    adding iframes and backup of results

--- web/modules/streamFinder_web.py
+++ web/modules/streamFinder_web.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3.7
 import sys
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, url_for
 from functools import wraps
 import json
-sys.path.insert(1, "/var/www/html/streamFinder/")
+import uuid
+import re
+import pathlib
+
+path = str(pathlib.Path(__file__).parent.absolute()).split("streamFinder")[0]
+sys.path.insert(1, path+"streamFinder/")
 from sources.main import search_movie
-sys.path.insert(1, "/var/www/html/streamFinder/web/modules/")
+sys.path.insert(1, path+"streamFinder/web/modules/")
+
 app = Flask(__name__)
 
 def check_auth(username, password):
@@ -52,12 +58,79 @@ def resultat():
 
     links_garb, links_ext, raw_websites = search_movie(title, sources=sources, lang=lang, nbRes=30 )
 
-    print("Search for", title, links_garb, links_ext, raw_websites)
+    #print("Search for", title, links_garb, links_ext, raw_websites)
     links_ext = [(erf,erf.split(";")[1]) for erf in links_ext]
+    # uniq id
+    id_ = str(uuid.uuid4().hex)
+
+    print(f"writting /tmp/streamFinder_{id_}.log")
+    results = {"title": title,
+            "garb": list(links_garb),
+            "ext": list(links_ext),
+            "raw": list(raw_websites),
+            "id": id_}
+    with open(f"/tmp/streamFinder_{id_}.log", "w") as f:
+        print(results)
+        f.write(json.dumps(results))
+    return redirect(url_for('.get_results', id=id_))
+
+@app.route("/results", methods = ['GET'])
+@requires_auth
+def get_results():
+    id_ = str(request.args.get("id"))
+    id_ = re.findall("[A-Za-z\d]+", id_)
+    if not id_:
+        return render_template("home.html")
+    id_ = id_[0]
+    table = {"garb": [],
+            "ext": [],
+            "raw": []}
+    try:
+        with open(f"/tmp/streamFinder_{id_}.log", "r") as f:
+            try:
+                table.update(json.loads(f.read()))
+                title = table["title"]
+            except Exception as e :
+                print(f"Error in retrieving data -> {e}")
+                links = []
+                title = "Couldn't find your results with this id"
+    except FileNotFoundError :
+        print(f"Tried to find file with id {id_} but failed")
+        title = "Couldn't find your results with this id"
     return render_template("resultat.html",
-                            resultat_garb=links_garb,
-                            resultat_ext=links_ext,
-                            resultats_raw=raw_websites,
+                            resultat_garb=table["garb"],
+                            resultat_ext=table["ext"],
+                            resultats_raw=table["raw"],
+                            iframes_id=id_,
+                            name=title)
+
+@app.route("/iframes", methods = ['GET'])
+@requires_auth
+def iframes():
+
+    id_ = str(request.args.get("id"))
+    id_ = re.findall("[A-Za-z\d]+", id_)
+    if not id_:
+        return render_template("home.html")
+    else:
+        id_ = id_[0]
+    try:
+        with open(f"/tmp/streamFinder_{id_}.log", "r") as f:
+            try:
+                table = json.loads(f.read())
+                links = table["garb"]
+                title = table["title"]
+            except Exception as e :
+                print(f"Error in retrieving data -> {e}")
+                links = []
+                title = "Couldn't find your results with this id"
+    except FileNotFoundError :
+        print(f"Tried to find file with id {id_} but failed")
+        links = []
+        title = "Couldn't find your results with this id"
+    return render_template("iframes.html",
+                            id=id_,
+                            links=links,
                             name=title)
 
 
--- web/streamFinder_web.wsgi
+++ web/streamFinder_web.wsgi
@@ -1,8 +1,10 @@
 #!/usr/bin/python3.7
 import sys
 import logging
+import pathlib
 
-sys.path.insert(1, "/var/www/html/streamFinder/web/modules")
+path = str(pathlib.Path(__file__).parent.absolute()).split("streamFinder")[0]
+sys.path.insert(1, path+"streamFinder/web/modules")
 from streamFinder_web import app as application
 logging.basicConfig(stream=sys.stderr)
 application.secret_key = '1234'
