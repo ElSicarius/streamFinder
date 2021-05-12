13ef0e642d675fbb3a1510fe70b2d800e71ac9eb
ElSicarius <amurat654@gmail.com>
2021-05-12 08:25:20 +0200

    symplify google search

--- sources/utils.py
+++ sources/utils.py
@@ -28,19 +28,19 @@ def duckduckgo_this(keywords: str, max_results: int=15) -> set:
             break
     return res
 
-def google_this(what: str, row: int, n: int, offset: int=0, SafeSearch="off", lang="fr", tld="fr"):
-    print(f"Searching for {what}")
+def google_this(what: str, stop: int, lang="fr", tld="com"):
     try:
-        res = search(what, tld=tld, num=row, start=offset, stop=n, pause=5, lang=lang, safe=SafeSearch, extra_params={'filter': '0'})
+        res = search(what, tld=tld, stop=stop, lang=lang, safe="off", extra_params={'filter': '0'}, verify_ssl=False)
     except error.HTTPError:
         print("Google cooldown... you need a to change your IP")
     return res
 
 
-def is_not_garbage(url: str) -> bool:
-    if any([True for element in consts.black_list_websites if element in url ]):
-        return False
-    return True
+def is_garbage(url: str) -> bool:
+    for element in consts.black_list_websites:
+        if element in url:
+            return True
+    return False
 
 
 def get_external_urls(title:str) -> set:
